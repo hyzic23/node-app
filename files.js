02cefcbb5df94ba7cf17669635ec6b5119d99699
hyzic <isaac.oghenevwapo@gmail.com>
2023-07-13 01:55:26 -0400

    Creating Writing Deleting files with Nodejs

---
+++ files.js
@@ -0,0 +1,45 @@
+const { error } = require('console');
+const fs = require('fs');
+
+//reading files
+// fs.readFile('./docs/blog1.txt', (error, data) => {
+//     if(error){
+//         console.log(error);
+//     }
+//         console.log(data.toString());
+// });
+
+//Writing files
+// fs.writeFile('./docs/blog1.txt', 'Hello, World', () => {
+//     console.log('File was written');
+// });
+
+
+//directories
+if(!fs.existsSync('./assets'))
+{
+    fs.mkdir('./assets', (error) => {
+        if(error){
+            console.error(error);
+        }
+            console.log('Folder created');
+    });
+}else {
+    fs.rmdir('./assets', (error) => {
+        if(error){
+            console.log(error);
+        }
+            console.log('Folder deleted');
+    });
+}
+
+
+// deleting files
+if(fs.existsSync('./docs/deleteme.txt')){
+    fs.unlink('./docs/deleteme.txt', (error) => {
+        if(error){
+            console.log(error);
+        }
+            console.log('file deleted');
+    });
+}
---
+++ people.js
@@ -0,0 +1,10 @@
+
+const people = ['Yoshi', 'Ryu', 'Chun-li', 'Mario'];
+const ages = [20, 25, 30, 35];
+
+console.log(people);
+console.log(ages);
+
+module.exports = {
+    people, ages
+};
