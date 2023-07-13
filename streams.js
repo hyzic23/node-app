5dc24ed6da590a332a116f3852d7b2dc960b5172
hyzic <isaac.oghenevwapo@gmail.com>
2023-07-13 02:18:58 -0400

    Add method to WriteStreams to files

--- streams.js
+++ streams.js
@@ -1,8 +1,21 @@
 const fs = require('fs');
 
 const readStream = fs.createReadStream('./docs/blog3.txt', { encoding: 'utf8'});
+const writeStream = fs.createWriteStream('./docs/blog4.txt');
 
+//WriteStream
+readStream.on('data', (chunk) => {
+    console.log('------- NEW CHUNK -------')
+    console.log(chunk);
+    writeStream.write('\nNEW CHUNK\n');
+    writeStream.write(chunk);
+});
+
+
+
+//ReadStream
 readStream.on('data', (chunk) => {
     console.log('------- NEW CHUNK -------')
     console.log(chunk);
 });
+
