27af05759c755643df4ede00f24dac37ebd3a19b
hyzic <isaac.oghenevwapo@gmail.com>
2023-07-14 01:14:38 -0400

    Created ejs pages changes the route method and added form page

--- app.js
+++ app.js
@@ -3,25 +3,28 @@ const express = require('express');
 // express app
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+
 //Listen for request
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    //res.send('<p>Home page</p>');
-    res.sendFile('./views/index.html', { root: __dirname});
+    //res.sendFile('./views/index.html', { root: __dirname});
+    res.render('index');
 });
 
 app.get('/about', (req, res) => {
-    //res.send('<p>About page</p>');
-    res.sendFile('./views/about.html', { root: __dirname});
+    //res.sendFile('./views/about.html', { root: __dirname});
+    res.render('about');
 });
 
 // Redirects
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+    res.render('create');
 });
 
 // 404 Page
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname});
+    res.status(404).render('404');
 });
