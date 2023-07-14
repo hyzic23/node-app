c626e145492eda1c7c12758d204de878637a2c08
hyzic <isaac.oghenevwapo@gmail.com>
2023-07-14 13:35:51 -0400

    connected to mongoDB database with mongoose

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require('express');
-var morgan = require('morgan')
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 // express app
 const app = express();
+
+// connect to mongoDb database
+const dbURI = 'mongodb+srv://sa:localdb23@node-cluster.i6lttna.mongodb.net/node-tut?retryWrites=true&w=majority';
+mongoose.connect(dbURI)
+        .then((res) => app.listen(3000))
+        .catch((err) => console.log(err));
+//mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true});
 
 // register view engine
 app.set('view engine', 'ejs');
 
 //Listen for request
-app.listen(3000);
+//app.listen(3000);
 
 // middleware and static file
 app.use(express.static('public'));
