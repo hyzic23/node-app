5c4759a20839df4e27fa784c19a7df7fc634cff2
hyzic <isaac.oghenevwapo@gmail.com>
2023-07-17 22:49:57 -0400

    Added login method and passord decrypt to validate when its being called

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,6 +4,7 @@ const authController= require('../controllers/authController');
 const router = express.Router();
 
 router.post('/register', authController.register);
+router.post('/login', authController.login);
 router.get('/dummy', authController.dummyApi);
 
 module.exports = router;
